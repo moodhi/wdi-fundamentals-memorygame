7c4f6931a3d4a6f67a2db162750501e69dec8e8e
unknown <modialmutairi.20@gmail.com>
2018-10-24 00:20:47 +0300

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,7 +1,32 @@
 
 
+//creating an object for each card that will store that card's 
+var cards =[
+{
+rank :"queen",
+suit : "hearts",
+cardImage :"images/images/queen-of-hearts.png"
+},
 
-var cards =["queen" , "king" , "king" , "king"];
+{
+rank :"queen",
+suit : "diamonds",
+cardImage :"images/images/queen-of-diamonds.png"
+},
+
+{
+rank :"king",
+suit : "hearts",
+cardImage :"images/images/king-of-hearts.png"
+},
+
+{
+rank :"king",
+suit: "diamonds",
+cardImage :"images/images/king-of-diamonds.png"
+}
+
+];
 
 //create an empty array.This will eventually hold the cards that are in play.
 var cardsInPlay = [];
@@ -19,18 +44,28 @@ var flipCard = function(cardId){
 	
 var carOne;
 var cardTwo;
+var suit;
+var cardImage;
 
 var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped  " +  cardOne);
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped  " + cards[cardId].rank);
 
+
+//this code is accessing the entire first object in the cards
 var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped  " +  cardTwo);
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
+
+
+console.log(cards[cardId].cardImage);
+
+
+console.log(cards[cardId].suit);
 
 checkForMatch();
 
 }
 
   flipCard(0);
-  //flipCard(2);
+  flipCard(2);
